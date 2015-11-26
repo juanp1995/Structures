11399a84b9828ac0bba360a5bf4e8732a5d237c7
Juan Pablo Brenes <juanp1995@gmail.com>
2015-11-26 15:17:26 -0600

    Structures

--- main.cpp
+++ main.cpp
@@ -1,47 +1,6 @@
-#include <QDebug>
-#include "Lists/Stack.h"
 
-int _order = 5;
-int getChildIndex(int *pArray, int pKey){
-
-}
 
 int main(){
-
-//	int *arr = new int[_order-ONE];
-//	arr[0] = 5;
-//	arr[1] = 10;
-//	arr[2] = 15;
-//	arr[3] = 20;
-//	arr[4] = 25;
-//	arr[5] = 30;
-//	arr[6] = 35;
-//	arr[7] = -1;
-//	arr[8] = -1;
-//	arr[9] = -1;
-
-
-//	deleteKey(arr, 5);
-//	deleteKey(arr, 10);
-//	deleteKey(arr, 15);
-//	deleteKey(arr, 20);
-//	deleteKey(arr, 25);
-//	deleteKey(arr, 30);
-//	deleteKey(arr, 35);
-
-//	for(int i=0; i<(8-1); i++){
-//		qDebug() << arr[i];
-//	}
-
-
-    Stack<int> *s = new Stack<int>();
-    s->push(5);
-    s->push(10);
-    s->push(15);
-    s->push(20);
-
-
-
 
 
 }
